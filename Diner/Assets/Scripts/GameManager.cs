7e5c8703d0013247da1cb6390f209c8ead1087a2
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-05-13 19:27:34 +0100

    Add chairs to scene

--- Diner/Assets/Scripts/GameManager.cs
+++ Diner/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
@@ -143,6 +144,6 @@ public class GameManager : MonoBehaviour
 
     public void ButtonBehaviour()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
